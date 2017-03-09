25c1a41b4ace04666d396bc9385651235d494f1b
Marliana Lara <Marliana@Marlianas-MacBook-Pro.local>
2017-03-08 20:54:24 -0500

    Add function to check for wins

--- main.js
+++ main.js
@@ -1,48 +1,66 @@
 document.addEventListener("DOMContentLoaded", function(event){
   const button = document.querySelector('button');
-  const bank = document.querySelector('.bank');
+  const bankEl = document.querySelector('.bank');
   const squares = document.querySelectorAll('.square');
   const resultDisplay = document.querySelector('.result');
+  const state = {"👽": 0, "🦁": 0, "💩": 0, "😻": 0, "🤖": 0, "🤡": 0}
+  let bank = 10;
 
   button.addEventListener('click', randomSquare);
   squares.forEach(square => square.addEventListener('click', (e) => {
-    bet(e);
+    placeBet(e);
   }));
 
-  function bet(e) {
-    var bankBalance = parseInt(bank.innerText);
+  function placeBet(e) {
+    var face = e.target.firstElementChild.innerText;
 
-    if(bankBalance == 0) {
-      return;
-    } else {
-      addBet(e)
+    if(bank > 0) {
+      addBet(e, face)
       balanceBank();
     }
   }
 
-  function addBet(e) {
-    var amount = parseInt(e.target.lastElementChild.innerHTML);
-    amount += 1;
-    e.target.lastElementChild.innerHTML = amount;
+  function addBet(e, face) {
+    state[face] += 1
+    e.target.lastElementChild.innerHTML = state[face];
   }
 
   function balanceBank() {
-    var bankBalance = parseInt(bank.innerText);
-    bankBalance -= 1;
-    bank.innerText = bankBalance;
+    bank -= 1;
+    bankEl.innerText = bank;
   }
-
 
   function randomSquare() {
-    const result = [];
-    for (var i = 0; i < 3; i++) {
-      var index = Math.floor(Math.random() * squares.length);
-      var randomSquare = squares[index].firstElementChild.innerHTML;
-      result.push(randomSquare);
+    const hand = [];
+    for (var roll = 0; roll < 3; roll++) {
+      var index = rand(0, 5);
+      var square = squares[index].firstElementChild.innerText;
+      hand.push(square);
     }
-    resultDisplay.innerHTML = result;
+    resultDisplay.innerHTML = hand;
+    winnings(hand);
   }
 
+  function rand(m, n) {
+    return m + Math.floor((n - m + 1) * Math.random());
+  }
 
+  function winnings(hand) {
+    var winnings = 0;
+    for (var die = 0; die < hand.length; die++) {
+      var face = hand[die];
+      // Find amount for face
+      var betAmount = state[face];
+      // If > 0 add value to bank total
+
+      if (betAmount > 0) {
+        bank += betAmount;
+        bankEl.innerText = bank;
+      }
+    }
+    // Reset tiles to 0
+    Object.keys(state).forEach( key => state[key] = 0 )
+    squares.forEach(square => square.lastElementChild.innerText = 0)
+  }
 
 });
